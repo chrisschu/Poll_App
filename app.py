bd5ff4e6e97bf6ccee01b9876a2324b53b572581
chrisschu <chschusc@edu.aau.at>
2020-01-11 22:28:33 +0100

    Alpha_0.3_16.01.2020

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-import pymongo
 import datetime
-from flask import Flask, render_template, request, jsonify
+
+import pymongo
+from flask import Flask, render_template, request
 from pymongo import *
 
 ##from flask_wtf import Form
@@ -74,17 +75,22 @@ def my_form():
 @app.route('/my_new_form', methods=['POST', 'GET'])
 def my_new_form():
     new = {
-        "question1": str(request.form.get('q4_overallSatisfaction[0]')),
-        "question2": str(request.form.get('q4_overallSatisfaction[1]')),
-        "question1": str(request.form.get('q4_overallSatisfaction[0]')),
-        "question2": str(request.form.get('q4_overallSatisfaction[1]')),
-        "dropdown1": str(request.form.get('dropdown1')),
-        "dropdown2": str(request.form.get('dropdown2')),
+        "pre_movie_0": str(request.form.get('q4_overallSatisfaction[0]')),
+        "pre_movie_1": str(request.form.get('q4_overallSatisfaction[1]')),
+        "pre_movie_2": str(request.form.get('q4_overallSatisfaction[2]')),
+        "pre_movie_3": str(request.form.get('q4_overallSatisfaction[3]')),
+        "pre_movie_4": str(request.form.get('q4_overallSatisfaction[4]')),
+        "suggested_movie_1": str(request.form.get('q20_movies20[0]')),
+        "suggested_movie_2": str(request.form.get('q20_movies20[1]')),
+        "suggested_movie_3": str(request.form.get('q20_movies20[2]')),
+        "suggested_movie_4": str(request.form.get('q20_movies20[3]')),
+        "suggested_movie_5": str(request.form.get('q20_movies20[4]')),
+        "suggested_movie_6": str(request.form.get('q20_movies20[5]')),
         "favouriteMovie": str(request.form.get('q23_bestRecommended')),
+        "poll_q1": str(request.form.get('q26_theItems')),
+        "poll_q2": str(request.form.get('q27_someOf')),
+        "poll_q3": str(request.form.get('q28_theItems28')),
         "date_page_1": datetime.datetime.utcnow(),
-        "date_page_2": datetime.datetime.utcnow(),
-        "date_page_3": datetime.datetime.utcnow(),
-        "date_page_4": datetime.datetime.utcnow(),
         "date_page_final": datetime.datetime.utcnow(),
     }
 
