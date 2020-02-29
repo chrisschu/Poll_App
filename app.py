65dc1406afd08371f1799d54e40564326353fdaa
chrisschu <chschusc@edu.aau.at>
2020-03-01 00:00:11 +0100

    CHANGES: changes to questionnaire / still not working changes to suggest_movies / still not working
    NEXT:
    little design changes
    "You liked" - Feature working

--- app.py
+++ app.py
@@ -21,14 +21,17 @@ genre = []
 # write here how many movies should displayed/used at the page
 pref_numMovies = 50
 sugg_numMovies = 10
+quest_num = 5
 
 #
 preferred_movies = []
 suggested_movies = []
+questions = []
 
 #
 allprefmovies = []
 allsuggmovies = []
+allquestions = []
 
 # for the two movies likes of the user
 user_likes_movies = []
@@ -40,16 +43,21 @@ date_page_rec_movie_3 = ''
 date_page_questionnaire_4 = ''
 
 # variables for rating the "suggested" movies, (Like/Dislike)
-suggested_movie_1 = 'X'
-suggested_movie_2 = 'X'
-suggested_movie_3 = 'X'
-suggested_movie_4 = 'X'
-suggested_movie_5 = 'X'
-suggested_movie_6 = 'X'
-suggested_movie_7 = 'X'
-suggested_movie_8 = 'X'
-suggested_movie_9 = 'X'
-suggested_movie_10 = 'X'
+# suggested_movie_1 = 'X'
+# suggested_movie_2 = 'X'
+# suggested_movie_3 = 'X'
+# suggested_movie_4 = 'X'
+# suggested_movie_5 = 'X'
+# suggested_movie_6 = 'X'
+# suggested_movie_7 = 'X'
+# suggested_movie_8 = 'X'
+# suggested_movie_9 = 'X'
+# suggested_movie_10 = 'X'
+
+# list for all the movies in the watchlist
+watchlist = [
+
+]
 
 # variable for the "star" movie, gets the movie title
 favourite = 'X'
@@ -207,9 +215,10 @@ def pref_movies():
     ##pre_movie_0 = str(request.form.get('q4_overallSatisfaction[0]'))
     # preferred_movies.append(str(request.form.get('q_pref_movies[1]')))
 
-    # to get the html input, passed in this list variable
-    for n in range(1, pref_numMovies):
-        preferred_movies.append(str(request.form.get('q_pref_movies[' + str(n) + ']')))
+    if request.method == 'POST':
+        # to get the html input, passed in this list variable
+        for n in range(1, pref_numMovies):
+            preferred_movies.append(str(request.form.get('q_pref_movies[' + str(n) + ']')))
 
     # variable geht die bewertete movieliste durch und speichert index/number in array.
     # this array will be used for displaying the movie in the next page
@@ -252,9 +261,7 @@ def pref_movies():
 @app.route('/rec_movies_1.html', methods=['POST', 'GET'])
 def rec_movies_1():
     # list with 10 objects
-    global suggested_movie_1, suggested_movie_2, suggested_movie_3, suggested_movie_4, suggested_movie_5, \
-        suggested_movie_6, suggested_movie_7, suggested_movie_8, suggested_movie_9, suggested_movie_10, favourite, \
-        date_page_rec_movie_3, page, sugg_numMovies, suggested_movies, allsuggmovies
+    global favourite, date_page_rec_movie_3, page, sugg_numMovies, suggested_movies, allsuggmovies, watchlist
 
     global user_likes_movies
 
@@ -274,7 +281,8 @@ def rec_movies_1():
     random.shuffle(allsuggmovies)
     # for x in range(0, sugg_numMovies):
     #    titles.append() = allsuggmovies.split
-    print(allsuggmovies)
+    print("Länge" + str(len(allsuggmovies)))
+    print("allsugmovies" + str(allsuggmovies))
     # print(allsuggmovies.pop())
     # suggested_movies = []
     # for x in range(0, sugg_numMovies):
@@ -282,10 +290,13 @@ def rec_movies_1():
     #    print('exit')
     date_page_rec_movie_3 = datetime.datetime.utcnow()
 
-    for n in range(0, sugg_numMovies):
-        suggested_movies.append(str(request.form.get('q20_movies20[' + str(n) + ']')))
+    if request.method == 'POST':
+        for n in range(0, sugg_numMovies):
+            suggested_movies.append(str(request.form.get('q20_movies20[' + str(n) + ']')))
+        watchlist = request.form.getlist('q_Watchlist')
+        favourite = str(request.form.get('q23_bestRecommended'))
 
-    favourite = str(request.form.get('q23_bestRecommended'))
+    print("Watchlist:" + str(watchlist))
 
     if request.method == 'POST':
         return redirect(url_for('questionnaire'))
@@ -330,17 +341,36 @@ def rec_movies_2():
 @app.route('/questionnaire.html', methods=['POST', 'GET'])
 def questionnaire():
     global poll_q1, poll_q2, poll_q3, date_page_questionnaire_4
+    global questions, quest_num, allquestions
 
     date_page_rec_movie_4 = datetime.datetime.utcnow()
 
-    poll_q1 = str(request.form.get('q26_theItems'))
-    poll_q2 = str(request.form.get('q27_someOf'))
-    poll_q3 = str(request.form.get('q28_theItems28'))
+    dbsurvey = db['survey']
+
+    allquestions.clear()
+    questions.clear()
+    # just one query for testing, [deactivated]
+    # name1 = dbmovies.find_one({'title': 'Interstellar'})
+
+    # querying all titles
+    # for x in range(0, sugg_numMovies):
+    for questions in dbsurvey.find({"_id": False}):
+        allquestions.append(questions)
+        print(questions)
+
+    for n in range(0, quest_num):
+        questions.append(str(request.form.get('q_survey_' + str(n) + ']')))
+
+    print("Länge" + str(len(allquestions)))
+    print("allsugmovies" + str(allquestions))
+
+    print("all questions: " + str(allquestions))
+    print("questions: " + str(questions))
 
     if request.method == 'POST':
         return redirect(url_for('submit'))
 
-    return render_template('/questionnaire.html', thing_to_say='Click here to start')
+    return render_template('/questionnaire.html', questions=allquestions)
 
 
 @app.route('/submit.html', methods=['POST', 'GET'])
@@ -353,15 +383,15 @@ def my_new_form():
     ## Assignment of the resulted input of the html variables
 
     global page, poll_q1, poll_q2, poll_q3, suggested_movies, sugg_numMovies, preferred_movies, pref_numMovies
-
     global date_page_task_description_1, date_page_pref_movie_2, date_page_rec_movie_3, date_page_questionnaire_4
+    global watchlist
 
     date_page_submit_5 = datetime.datetime.utcnow()
 
     print(preferred_movies)
 
     ## fuction for saving all data in mongo database
-    save(suggested_movies, page, preferred_movies, favourite, poll_q1, poll_q2, poll_q3,
+    save(suggested_movies, page, preferred_movies, favourite, poll_q1, poll_q2, poll_q3, watchlist,
          date_page_task_description_1, date_page_pref_movie_2, date_page_rec_movie_3, date_page_questionnaire_4,
          date_page_submit_5)
 
@@ -375,7 +405,7 @@ def my_new_form():
 
 
 def save(suggested_movies_x, page_x, preferred_movies_x,
-         favourite_x, poll_q1_x, poll_q2_x, poll_q3_x, date_page_task_description_1_x,
+         favourite_x, poll_q1_x, poll_q2_x, poll_q3_x, watchlist_x, date_page_task_description_1_x,
          date_page_pref_movie_2_x, date_page_rec_movie_3_x, date_page_questionnaire_4_x,
          date_page_submit_5_x):
     """
@@ -390,12 +420,7 @@ def save(suggested_movies_x, page_x, preferred_movies_x,
     pref_movie_attributename = []
     sugg_movie_attributename = []
 
-    # print(preferred_movies)
-    pref_movie = []
-
-    # for the right part of the dict
-    # passes the variable
-    pref_movie = []
+    print("Watchlist!!!!!: " + str(watchlist))
 
     # https://docs.python.org/2/tutorial/datastructures.html#list-comprehensions
     for x in range(0, pref_numMovies):
@@ -404,10 +429,12 @@ def save(suggested_movies_x, page_x, preferred_movies_x,
     for x in range(0, sugg_numMovies):
         sugg_movie_attributename.append('sugg_movie_' + str(x))
 
-    print("pref attribute name" + str(pref_movie_attributename))
-    print("preferred movie x" + str(preferred_movies_x))
+    ##print("pref attribute name" + str(pref_movie_attributename)) TODO
+    ##print("preferred movie x" + str(preferred_movies_x))
 
     print(dict(zip(pref_movie_attributename, preferred_movies_x)))
+    print(dict(zip(sugg_movie_attributename, suggested_movies_x)))
+
     print("suggested movies" + str(suggested_movies))
 
     # using the dictionary form to input data in mongodb
@@ -425,6 +452,7 @@ def save(suggested_movies_x, page_x, preferred_movies_x,
     }
     new.update(dict(zip(pref_movie_attributename, preferred_movies_x)))
     new.update(dict(zip(sugg_movie_attributename, suggested_movies_x)))
+    new.update(watchlist_x)
     try:
         ## Creates a new document in DB
         # creates a new ID for json-file in mongodb
